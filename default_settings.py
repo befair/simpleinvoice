689b5c447bf329a3d90982b181189632d62d12b3
Luca Ferroni <feroda@users.sourceforge.net>
2010-04-08 13:58:42 +0200

    Fixed installed apps module string

--- default_settings.py
+++ default_settings.py
@@ -89,6 +89,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'invoice',
-    'admin',
+    'simpleinvoice.invoice',
+    'simpleinvoice.admin',
 )
