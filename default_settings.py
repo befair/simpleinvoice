b17325972ed45158519a04c9adc67de20cf76095
Luca Ferroni <feroda@users.sourceforge.net>
2010-04-08 15:13:14 +0200

    Fixed context_processor module string

--- default_settings.py
+++ default_settings.py
@@ -68,7 +68,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
-    'context_processor.common_processor',
+    'simpleinvoice.context_processor.common_processor',
 )
 
 MIDDLEWARE_CLASSES = (
