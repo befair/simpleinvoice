650883f809fb98b8738a90f6067f234d0573b183
Luca Ferroni <feroda@users.sourceforge.net>
2010-04-08 14:10:22 +0200

    Fixed default DATABASE_NAME and removed custom settings.py from the index

--- default_settings.py
+++ default_settings.py
@@ -16,7 +16,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_NAME = os.path.join(BASE_DIR, 'invoice.db')  # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -92,3 +92,11 @@ INSTALLED_APPS = (
     'simpleinvoice.invoice',
     'simpleinvoice.admin',
 )
+
+COMPANY_VAT_PERCENT = 0.20
+COMPANY_NAME = "BeFair s.n.c."
+COMPANY_INTERNET_CONTACTS = "http://www.befair.it - info@befair.it"
+COMPANY_ADDRESS = "Viale Fanin, 48 - 40127 Bologna"
+COMPANY_CONTACTS = ""
+COMPANY_VAT_NUMBER = "02964711200"
+COMPANY_LOGO = "logo.jpg"
