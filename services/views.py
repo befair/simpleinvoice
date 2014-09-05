bb13b5c0e71e0de94dba3d717c3c7c360130067f
matteo88 <kingletti88@gmail.com>
2014-09-05 18:21:29 +0200

    API in bulk ServiceSubscriptionPayment creation

--- services/views.py
+++ services/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
+#from django.core.exceptions import DoesNotExist, IntegrityError
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils import timezone
+from django.db import IntegrityError
 
-from services.models import ServiceSubscription, Service
+from services.models import ServiceSubscriptionPayment, ServiceSubscription, Service
 from invoice.models import Customer
 
 import json
@@ -23,6 +27,66 @@ def bulk_service_payment(request):
 @staff_member_required
 @csrf_exempt
 def bulk_payments(request):
+    """
+    API view.
+    Create all payments from the in bulk insert.
+    """
+
+    qd  = request.POST.copy()
+    objects = []
+
+    try:
+        service_id = int(qd.pop('service_id')[0])
+    except KeyError as e:
+        return HttpResponse('{"status": "ERROR"}',content_type="application/json")
+
+    i = 0
+
+    while len(qd) > 0:
+        customer_id = int(qd.pop("payments[%s][%s]" % (i,'customer'))[0])
+        date = qd.pop("payments[%s][%s]" % (i,'paid_for'))[0]
+        try:
+            #TODO Matteo: check if date is valid
+            paid_for = timezone.datetime(int(date[6:]),int(date[3:5]),int(date[:2]))
+        except ValueError as e:
+            return HttpResponse('{"status": "ERROR","message": "%(message)s' % {'message':_("Invalid data"),},content_type="application/json")
+            
+        amount = float(qd.pop("payments[%s][%s]" % (i,'cost'))[0])
+        if amount < 0:
+            return HttpResponse('{"status": "ERROR","message": "%(message)s' % {'message':_("Invalid data"),},content_type="application/json")
+
+        notes = qd.pop("payments[%s][%s]" % (i,'notes'))[0]
+
+        ss = ServiceSubscriptionPayment()
+        try:
+            subscription = ServiceSubscription.objects.get(
+                service=Service.objects.get(pk=service_id),
+                customer=Customer.objects.get(pk=customer_id)
+            )
+        except DoesNotExist as e:
+            return HttpResponse('{"status": "ERROR","message": "%(message)s' % {'message':_("Invalid data"),},content_type="application/json")
+         
+        ss.subscription = subscription
+        ss.amount = amount
+        ss.vat_percent = subscription.vat_percent
+        ss.discount = subscription.discount
+        ss.paid_for = paid_for
+        ss.note = notes
+
+        objects.append(ss)
+
+        i += 1
+
+    for obj in objects:
+        try:
+            obj.save()
+        except IntegrityError as e:
+            return HttpResponse('{"status": "ERROR","message": "%(message)s' % {'message':_("Invalid data"),},content_type="application/json")
+
+        obj.subscription.last_paid_on = ss.paid_on
+        obj.subscription.last_paid_for = ss.paid_for
+        obj.subscription.save()
+
     return HttpResponse('a');
 
 @staff_member_required
