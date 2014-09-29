3e1cfb4cfda65f85641f98a5bc17520c6e7f1386
matteo88 <kingletti88@gmail.com>
2014-09-29 22:55:41 +0200

    [fix]: risolti i problemi per il salvataggio dei pagamenti in_bulk

--- services/tests.py
+++ services/tests.py
@@ -1,3 +1,205 @@
 from django.test import TestCase
+from django.test.client import Client
+
+from django.core.management import call_command
+from django.contrib.auth.models import User
+
+from django.core.urlresolvers import reverse
+
+from services.models import ServiceSubscription, ServiceSubscriptionPayment, DATE_CHOICES
+
+from lib import views_support
 
 # Create your tests here.
+
+class TestWithClient(TestCase):
+
+    USERNAME = ""
+    TEST_PASSWORD = ""
+
+    def setUp(self):
+
+        # Create test user
+        self._user = self.create_user(username=self.USERNAME)
+
+        self._c = Client()
+
+    def create_user(self, username=None):
+        """Make the user able to login."""
+        user = User.objects.create(
+            username=[self.USERNAME, username][bool(username)]
+        )
+        user.set_password(self.TEST_PASSWORD)
+        user.save()
+        return user
+
+    def create_user_unloggable(self, username=None):
+        """Create user with no password --> cannot login"""
+        user = User.objects.create(
+            username=[self.USERNAME, username][bool(username)]
+        )
+        return user
+
+    def _logout(self):
+        self._c.logout()
+
+    def _login(self, user=None):
+        """Login a user.
+
+        If user is None, login the default user
+        """
+
+        self._logout()
+        login_user = [self._user, user][bool(user)]
+        rv = self._c.login(username=login_user.username, password=self.TEST_PASSWORD)
+        return rv
+
+    def _check_for_error_response(self, response, e=Exception):
+        """HTTP response is always 200, context_data 'http_status_code' tells the truth"""
+        self.assertEqual(response.status_code, views_support.HTTP_SUCCESS)
+        self.assertEqual(
+            response.context_data['http_status_code'], 
+            views_support.HTTP_ERROR_INTERNAL
+        )
+        self.assertEqual(response.context_data['exception_type'], e)
+
+    def _check_for_success_response(self, response, is_ajax=True):
+        """HTTP response is always 200, context_data 'http_status_code' tells the truth"""
+        if is_ajax:
+            self.assertEqual(response.status_code, views_support.HTTP_SUCCESS)
+            self.assertEqual(
+                response.context_data['http_status_code'], 
+                views_support.HTTP_SUCCESS
+            )
+        else:
+            self.assertEqual(response.status_code, views_support.HTTP_SUCCESS)
+    
+    def _check_for_redirect_response(self,response, is_ajax=False):
+        """ HTTP response is 302, in case the server redirects to another page"""
+        if is_ajax:
+            self.assertEqual(response.status_code, views_support.HTTP_SUCCESS)
+            self.assertEqual(
+                response.context_data['http_status_code'], 
+                views_support.HTTP_REDIRECT
+            )
+        else:
+            self.assertEqual(response.status_code, views_support.HTTP_REDIRECT)
+
+    def _POST(self, url, is_ajax, **kwargs):
+        
+        if is_ajax:
+            response = self._c.post(url,
+                kwargs,
+                HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+            )
+        else:
+            response = self._c.post(url,
+                kwargs
+            )
+        return response
+
+    def _GET(self, url, is_ajax, **kwargs):
+
+        if is_ajax:
+            response = self._c.get(url,
+                kwargs,
+                HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+            )
+        else:
+            response = self._c.get(url,
+                kwargs
+            )
+        return response
+
+class TestCaseWithFixtures(TestWithClient):
+    ''' '''
+
+    DATE_FMT = '%Y-%m-%d %H:%M:%S%Z'
+
+    def setUp(self):
+        """
+        """
+        super(TestCaseWithFixtures, self).setUp()
+        call_command('loaddata', 'fixtures/test_data.json', verbosity=0)
+                
+                
+class ServiceSubscriptionPaymentTest(TestCaseWithFixtures):
+    ''' '''
+
+    def setUp(self):
+        super(ServiceSubscriptionPaymentTest, self).setUp()
+        print ServiceSubscription.all_objects.all()
+        self.subscription = ServiceSubscription.objects.first()
+        self.payment = ServiceSubscriptionPayment()
+        self.payment.subscription = self.subscription
+        self.payment.amount = self.subscription.service.amount
+        self.payment.vat_percent = self.subscription.vat_percent
+        self.payment.discount = self.subscription.discount
+        self.payment.paid_for = DATE_CHOICES[9][0].strftime(self.DATE_FMT)
+        self.payment.note = "note"
+        self.payment.save()
+        self.subscription.last_paid_on = self.payment.paid_on
+        self.subscription.last_paid_for = self.payment.paid_for
+
+    def _do_POST_service_subscripton_payment_delete(self, sub_payment, ajax=False, **kwargs):
+
+        response = self._POST(
+            reverse('services_servicesubscriptionpayment_delete', args=(sub_payment.pk,)),
+            ajax,
+            **kwargs
+        )
+        return response
+
+    def _do_POST_service_subscripton_payment_create(self, ajax=False, **kwargs):
+
+        response = self._POST(
+            reverse('services_servicesubscriptionpayment_add', args=()),
+            ajax,
+            **kwargs
+        )
+        return response
+
+    #def test_service_subscripton_payment_delete(self):
+    #    """
+    #    """
+    #   
+    #    sub_payment = ServiceSubscriptionPayment()
+    #    sub_payment.subscription = self.subscription
+    #    sub_payment.amount = self.subscription.service.amount
+    #    sub_payment.vat_percent = self.subscription.vat_percent
+    #    sub_payment.discount = self.subscription.discount
+    #    sub_payment.paid_for = DATE_CHOICES[10][0].strftime(self.DATE_FMT)
+    #    sub_payment.note = "note"
+    #    sub_payment.save()
+    #    self.subscription.last_paid_on = sub_payment.paid_on
+    #    self.subscription.last_paid_for = sub_payment.paid_for
+
+    #    self.assertEqual(sub_payment.paid_on,self.subscription.last_paid_on) 
+    #    self.assertEqual(sub_payment.paid_for,self.subscription.last_paid_for) 
+
+    #    sub_payment.delete()
+
+    #    self.assertEqual(self.payment.paid_on,self.subscription.last_paid_on) 
+    #    self.assertEqual(self.payment.paid_for,self.subscription.last_paid_for) 
+
+    def test_client_service_subscripton_payment_delete(self, payment=None, query_string=""):
+        """
+        """
+      
+        self._do_POST_service_subscripton_payment_create(
+            subscription = self.subscription,
+            amount = self.subscription.service.amount,
+            vat_percent = self.subscription.vat_percent,
+            discount = self.subscription.discount,
+            paid_for = DATE_CHOICES[10][0].strftime(self.DATE_FMT),
+            note = "note",
+        )
+
+        self.assertEqual(sub_payment.paid_on,self.subscription.last_paid_on) 
+        self.assertEqual(sub_payment.paid_for,self.subscription.last_paid_for) 
+
+        self._do_POST_service_subscripton_payment_delete(
+            sub_payment)
+
+        self.assertEqual(self.payment.paid_on,self.subscription.last_paid_on) 
+        self.assertEqual(self.payment.paid_for,self.subscription.last_paid_for) 
--- services/views.py
+++ services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
@@ -7,6 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.utils import timezone
 from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned, ValidationError
 
 from services.models import ServiceSubscriptionPayment, ServiceSubscription, Service, DATE_CHOICES
 from invoice.models import Customer
@@ -38,38 +39,55 @@ def bulk_payments(request):
     try:
         service_id = int(qd.pop('service_id')[0])
     except KeyError as e:
-        return HttpResponse('{"status": "ERROR"}',content_type="application/json")
+        return HttpResponseServerError('{"status": "ERROR", "message" : "Please select a service"}',content_type="application/json")
 
     i = 0
 
     while len(qd) > 0:
-        customer_id = int(qd.pop("payments[%s][%s]" % (i,'customer'))[0])
-        date = qd.pop("payments[%s][%s]" % (i,'paid_for'))[0]
+        subscription_id = None
         try:
-            #TODO Matteo: check if date is valid
-            #DONE Matteo
+            customer_id = int(qd.pop("payments[%s][%s]" % (i,'customer'))[0])
+        except KeyError as e:
+            subscription_id =  int(qd.pop("payments[%s][%s]" % (i,'subscription'))[0])
+        date = qd.pop("payments[%s][%s]" % (i,'paid_for'))[0]
+        amount = float(qd.pop("payments[%s][%s]" % (i,'cost'))[0])
+        try:
             if check_date:
                 paid_for = timezone.datetime(int(date[6:]),int(date[3:5]),int(date[:2]))
             else:
                 raise ValueError
         except ValueError as e:
-            return HttpResponse('{"status": "ERROR","message": "%(message)s' % {'message':_("Invalid data"),},content_type="application/json")
+            return HttpResponseServerError('{"status":"ERROR", "cost":"%(cost)s","customer":"%(customer_id)s", "message": "Please choose a valid date"}' % {"cost" : amount, "customer_id" : customer_id},content_type="application/json")
             
-        amount = float(qd.pop("payments[%s][%s]" % (i,'cost'))[0])
         if amount < 0:
-            return HttpResponse('{"status": "ERROR","message": "%(message)s' % {'message':_("Invalid data"),},content_type="application/json")
+            return HttpResponseServerError('{"status": "ERROR","cost":"%(cost)s","customer":"%(customer_id)s", "message": "Please insert a non negative amount"}' % {"cost" : amount, "customer_id" : customer_id},content_type="application/json")
 
         notes = qd.pop("payments[%s][%s]" % (i,'notes'))[0]
 
         ss = ServiceSubscriptionPayment()
         try:
-            subscription = ServiceSubscription.objects.get(
+            if not subscription_id:
+                subscription = ServiceSubscription.objects.get(
+                    service=Service.objects.get(pk=service_id),
+                    customer=Customer.objects.get(pk=customer_id)
+                )
+            else:
+                subscription = ServiceSubscription.objects.get(
+                    pk=subscription_id
+                )
+        except ObjectDoesNotExist as e:
+            return HttpResponseServerError('{"status": "ERROR","cost":"%(cost)s","customer":"%(customer_id)s", "message": "A subscription for service %(service)s  and customer %(customer)s does not exist"}' % {"service" : Service.objects.get(pk=service_id), "customer" : Customer.objects.get(pk=customer_id),"cost" : amount, "customer_id" : customer_id},content_type="application/json")
+        except MultipleObjectsReturned as e:
+            subscriptions = ServiceSubscription.objects.filter(
                 service=Service.objects.get(pk=service_id),
                 customer=Customer.objects.get(pk=customer_id)
             )
-        except DoesNotExist as e:
-            return HttpResponse('{"status": "ERROR","message": "%(message)s' % {'message':_("Invalid data"),},content_type="application/json")
-         
+            sub_list = ""
+            for sub in subscriptions:
+                sub_list = sub_list + "<option value='%s'>%s</option>" % (sub.pk,sub) 
+ 
+            return HttpResponseServerError('{"status": "ERROR","cost":"%(cost)s","customer":"%(customer_id)s", "sub_list":"%(sub_list)s", "message":"There are multiple subscription for service %(service)s and customer %(customer)s"}' % {"service" : Service.objects.get(pk=service_id), "customer" : Customer.objects.get(pk=customer_id),"cost" : amount, "customer_id" : customer_id, "sub_list" : sub_list},content_type="application/json")
+            
         ss.subscription = subscription
         ss.amount = amount
         ss.vat_percent = subscription.vat_percent
@@ -77,19 +95,25 @@ def bulk_payments(request):
         ss.paid_for = paid_for
         ss.note = notes
 
-        objects.append(ss)
+        objects.append({"saved":False,"obj":ss})
 
         i += 1
 
-    for obj in objects:
+    for pair in objects:
         try:
-            obj.save()
+            obj = pair['obj']
+            if not pair['saved']:
+                obj.save()
+                obj.subscription.last_paid_on = ss.paid_on
+                obj.subscription.last_paid_for = ss.paid_for
+                obj.subscription.save()
+                pair['saved'] = True
         except IntegrityError as e:
-            return HttpResponse('{"status": "ERROR","message": "%(message)s' % {'message':_("Invalid data"),},content_type="application/json")
+            return HttpResponseServerError('{"status": "ERROR","cost":"%(cost)s","customer":"%(customer_id)s","message": "Payment for subscription %(subscription)s has not been created"}' % {"subscription" : ServiceSubscription.objects.get(pk=obj.subscription.pk), "cost" : obj.amount, "customer_id" : obj.subscription.customer.pk},content_type="application/json")
+        except ValidationError as e:
+            return HttpResponseServerError('{"status": "ERROR","cost":"%(cost)s","customer":"%(customer_id)s","paid_for":"%(paid_for)s","message": "A payment for subscription  %(subscription)s already exists "}' % {"subscription" : ServiceSubscription.objects.get(pk=obj.subscription.pk), "cost" : obj.amount, "customer_id" : obj.subscription.customer.pk, "paid_for" : obj.paid_for} ,content_type="application/json")
 
-        obj.subscription.last_paid_on = ss.paid_on
-        obj.subscription.last_paid_for = ss.paid_for
-        obj.subscription.save()
+
 
     return HttpResponse('a');
 
