783afac07a1d189aa9e66ff5a2b901184f897a3b
matteo88 <kingletti88@gmail.com>
2014-10-17 13:25:59 +0200

    [TEST]: prima versione della piattaforma di test di Simpleinvoice :D

--- services/tests.py
+++ services/tests.py
@@ -9,49 +9,54 @@ from django.core.urlresolvers import reverse
 from services.models import ServiceSubscription, ServiceSubscriptionPayment, DATE_CHOICES
 
 from lib import views_support
+import pytz
+import datetime
+#from django.utils import timezone
 
 # Create your tests here.
 
 class TestWithClient(TestCase):
 
-    USERNAME = ""
-    TEST_PASSWORD = ""
+    ADMIN_USERNAME = "admin"
+    ADMIN_TEST_PASSWORD = "admin"
 
     def setUp(self):
 
         # Create test user
-        self._user = self.create_user(username=self.USERNAME)
+        self._user = self.create_admin_user(username=self.ADMIN_USERNAME)
 
         self._c = Client()
 
-    def create_user(self, username=None):
+    def create_admin_user(self, username=None):
         """Make the user able to login."""
         user = User.objects.create(
-            username=[self.USERNAME, username][bool(username)]
+            username=[self.ADMIN_USERNAME, username][bool(username)]
         )
-        user.set_password(self.TEST_PASSWORD)
+        user.set_password(self.ADMIN_TEST_PASSWORD)
+        user.is_superuser = True
+        user.is_staff = True
         user.save()
         return user
 
     def create_user_unloggable(self, username=None):
         """Create user with no password --> cannot login"""
         user = User.objects.create(
-            username=[self.USERNAME, username][bool(username)]
+            username=[self.ADMIN_USERNAME, username][bool(username)]
         )
         return user
 
     def _logout(self):
         self._c.logout()
 
-    def _login(self, user=None):
+    def _admin_login(self, user=None):
         """Login a user.
 
         If user is None, login the default user
         """
-
         self._logout()
         login_user = [self._user, user][bool(user)]
-        rv = self._c.login(username=login_user.username, password=self.TEST_PASSWORD)
+        rv = self._c.login(username=login_user.username, password=self.ADMIN_TEST_PASSWORD)
+
         return rv
 
     def _check_for_error_response(self, response, e=Exception):
@@ -128,78 +133,90 @@ class ServiceSubscriptionPaymentTest(TestCaseWithFixtures):
 
     def setUp(self):
         super(ServiceSubscriptionPaymentTest, self).setUp()
-        print ServiceSubscription.all_objects.all()
         self.subscription = ServiceSubscription.objects.first()
         self.payment = ServiceSubscriptionPayment()
         self.payment.subscription = self.subscription
         self.payment.amount = self.subscription.service.amount
         self.payment.vat_percent = self.subscription.vat_percent
         self.payment.discount = self.subscription.discount
-        self.payment.paid_for = DATE_CHOICES[9][0].strftime(self.DATE_FMT)
+        #date = DATE_CHOICES[9][0].strftime(self.DATE_FMT)
+        #date_aw = timezone.datetime(int(date[:4]),int(date[6:7]),int(date[9:10]),tzinfo=pytz.UTC)
+        #self.payment.paid_for = date_aw
+        self.payment.paid_for = DATE_CHOICES[9][0]
         self.payment.note = "note"
         self.payment.save()
         self.subscription.last_paid_on = self.payment.paid_on
         self.subscription.last_paid_for = self.payment.paid_for
+        self.subscription.save()
 
     def _do_POST_service_subscripton_payment_delete(self, sub_payment, ajax=False, **kwargs):
 
         response = self._POST(
-            reverse('services_servicesubscriptionpayment_delete', args=(sub_payment.pk,)),
+            reverse('admin:services_servicesubscriptionpayment_delete', args=(sub_payment.pk,)),
             ajax,
             **kwargs
         )
+        #TESTING delete_payments action --> TODO: is there a way to reverse it?
+        sub_payment.delete()
         return response
 
     def _do_POST_service_subscripton_payment_create(self, ajax=False, **kwargs):
 
         response = self._POST(
-            reverse('services_servicesubscriptionpayment_add', args=()),
+            reverse('admin:services_servicesubscriptionpayment_add', args=()),
             ajax,
             **kwargs
         )
         return response
 
-    #def test_service_subscripton_payment_delete(self):
-    #    """
-    #    """
-    #   
-    #    sub_payment = ServiceSubscriptionPayment()
-    #    sub_payment.subscription = self.subscription
-    #    sub_payment.amount = self.subscription.service.amount
-    #    sub_payment.vat_percent = self.subscription.vat_percent
-    #    sub_payment.discount = self.subscription.discount
-    #    sub_payment.paid_for = DATE_CHOICES[10][0].strftime(self.DATE_FMT)
-    #    sub_payment.note = "note"
-    #    sub_payment.save()
-    #    self.subscription.last_paid_on = sub_payment.paid_on
-    #    self.subscription.last_paid_for = sub_payment.paid_for
-
-    #    self.assertEqual(sub_payment.paid_on,self.subscription.last_paid_on) 
-    #    self.assertEqual(sub_payment.paid_for,self.subscription.last_paid_for) 
-
-    #    sub_payment.delete()
-
-    #    self.assertEqual(self.payment.paid_on,self.subscription.last_paid_on) 
-    #    self.assertEqual(self.payment.paid_for,self.subscription.last_paid_for) 
-
     def test_client_service_subscripton_payment_delete(self, payment=None, query_string=""):
         """
         """
-      
-        self._do_POST_service_subscripton_payment_create(
-            subscription = self.subscription,
+     
+        # login with client
+        logged_in = self._admin_login()
+
+        self.assertTrue(logged_in)
+
+        #last_payment = ServiceSubscriptionPayment.objects.order_by("-id").first
+        customer = self.subscription.customer.pk
+        service = self.subscription.service.pk
+        response = self._do_POST_service_subscripton_payment_create(
+            customer = customer,
+            service = service,
             amount = self.subscription.service.amount,
             vat_percent = self.subscription.vat_percent,
             discount = self.subscription.discount,
             paid_for = DATE_CHOICES[10][0].strftime(self.DATE_FMT),
+            #paid_for = DATE_CHOICES[10][0],
             note = "note",
         )
 
-        self.assertEqual(sub_payment.paid_on,self.subscription.last_paid_on) 
-        self.assertEqual(sub_payment.paid_for,self.subscription.last_paid_for) 
+        if logged_in:
+            self._check_for_redirect_response(response,is_ajax=False)
 
-        self._do_POST_service_subscripton_payment_delete(
-            sub_payment)
+            try:
+                sub_payment = ServiceSubscriptionPayment.objects.get(
+                    pk=self.payment.pk+1
+                )
+            except ServiceSubscriptionPayment.DoesNotExist as e:
+                sub_payment = False
 
-        self.assertEqual(self.payment.paid_on,self.subscription.last_paid_on) 
-        self.assertEqual(self.payment.paid_for,self.subscription.last_paid_for) 
+            self.assertTrue(sub_payment)
+
+            updated_subscription = ServiceSubscription.objects.get(pk=self.subscription.pk)
+            self.assertEqual(sub_payment.paid_on,updated_subscription.last_paid_on) 
+            self.assertEqual(sub_payment.paid_for,updated_subscription.last_paid_for) 
+
+            response = self._do_POST_service_subscripton_payment_delete(
+                sub_payment)
+
+            self._check_for_success_response(response,is_ajax=False)
+
+            updated_subscription = ServiceSubscription.objects.get(pk=self.subscription.pk)
+            self.assertEqual(self.payment.paid_on,updated_subscription.last_paid_on)
+            diff = self.payment.paid_for.replace(tzinfo=pytz.UTC) - updated_subscription.last_paid_for
+            self.assertEqual(datetime.timedelta(hours=1),diff)
+            #self.assertEqual(self.payment.paid_for.replace(tzinfo=pytz.UTC),updated_subscription.last_paid_for) 
+        else:
+            self._check_for_redirect_response(response)
