739f13ac120411982d064dc39a49c8b1d6851c2e
matteo88 <kingletti88@gmail.com>
2014-10-20 17:32:18 +0200

    [TEST]: aggiunti casi di test per creazione di Servizio, di Sottoscrizione e di Pagamento

--- services/tests.py
+++ services/tests.py
@@ -6,7 +6,15 @@ from django.contrib.auth.models import User
 
 from django.core.urlresolvers import reverse
 
-from services.models import ServiceSubscription, ServiceSubscriptionPayment, DATE_CHOICES
+from services.models import (
+    Service,
+    ServiceSubscription, 
+    ServiceSubscriptionPayment, 
+    DATE_CHOICES,
+    UNIT_MONTHS
+)
+
+from invoice.models import Customer
 
 from lib import views_support
 import pytz
@@ -120,6 +128,7 @@ class TestCaseWithFixtures(TestWithClient):
     ''' '''
 
     DATE_FMT = '%Y-%m-%d %H:%M:%S+00:00'
+    DATE_FMT_YMG = '%Y-%m-%d'
 
     def setUp(self):
         """
@@ -133,6 +142,9 @@ class ServiceSubscriptionPaymentTest(TestCaseWithFixtures):
 
     def setUp(self):
         super(ServiceSubscriptionPaymentTest, self).setUp()
+
+        self.customer = Customer.objects.first()
+        self.service = Service.objects.first()
         self.subscription = ServiceSubscription.objects.first()
         self.payment = ServiceSubscriptionPayment()
         self.payment.subscription = self.subscription
@@ -150,6 +162,8 @@ class ServiceSubscriptionPaymentTest(TestCaseWithFixtures):
         self.subscription.save()
 
     def _do_POST_service_subscripton_payment_delete(self, sub_payment, ajax=False, **kwargs):
+        """
+        """
 
         response = self._POST(
             reverse('admin:services_servicesubscriptionpayment_delete', args=(sub_payment.pk,)),
@@ -161,6 +175,8 @@ class ServiceSubscriptionPaymentTest(TestCaseWithFixtures):
         return response
 
     def _do_POST_service_subscripton_payment_create(self, ajax=False, **kwargs):
+        """
+        """
 
         response = self._POST(
             reverse('admin:services_servicesubscriptionpayment_add', args=()),
@@ -168,6 +184,171 @@ class ServiceSubscriptionPaymentTest(TestCaseWithFixtures):
             **kwargs
         )
         return response
+
+    def _do_POST_service_subscripton_delete(self, subscription, ajax=False, **kwargs):
+        """
+        """
+
+        response = self._POST(
+            reverse('admin:services_servicesubscription_delete', args=(subscription.pk,)),
+            ajax,
+            **kwargs
+        )
+        #TODO: here delete_subscriptions admin action should be tested
+        return response
+
+    def _do_POST_service_subscripton_create(self, ajax=False, **kwargs):
+        """
+        """
+
+        response = self._POST(
+            reverse('admin:services_servicesubscription_add', args=()),
+            ajax,
+            **kwargs
+        )
+        return response
+
+    def _do_POST_service_delete(self, service, ajax=False, **kwargs):
+        """
+        """
+
+        response = self._POST(
+            reverse('admin:services_service_delete', args=(service.pk,)),
+            ajax,
+            **kwargs
+        )
+        return response
+
+    def _do_POST_service_create(self, ajax=False, **kwargs):
+        """
+        """
+
+        response = self._POST(
+            reverse('admin:services_service_add', args=()),
+            ajax,
+            **kwargs
+        )
+        return response
+
+    def test_client_service_create(self, query_string=""):
+        """
+        """
+     
+        # login with client
+        logged_in = self._admin_login()
+
+        self.assertTrue(logged_in)
+
+        last_serv_pk = Service.objects.count()
+
+        response = self._do_POST_service_create(
+            abbreviation = "serv",
+            name = "Servizio test",
+            description = "Servizio di test",
+            period = 12,
+            period_unit_raw = UNIT_MONTHS,
+            period_unit_display = UNIT_MONTHS,
+            amount = 40,
+            default_vat_percent = 0.22,
+        )
+
+        if logged_in:
+            self._check_for_redirect_response(response,is_ajax=False)
+
+            try:
+                service = Service.objects.get(
+                    pk=last_serv_pk+1
+                )
+            except Service.DoesNotExist as e:
+                service = False
+
+            self.assertTrue(service)
+
+        else:
+            self._check_for_redirect_response(response)
+
+    def test_client_service_subscripton_create(self, query_string=""):
+        """
+        """
+     
+        # login with client
+        logged_in = self._admin_login()
+
+        self.assertTrue(logged_in)
+
+        last_sub_pk = ServiceSubscription.all_objects.count()
+
+        customer = self.customer.pk
+        service = self.service.pk
+        response = self._do_POST_service_subscripton_create(
+            customer = customer,
+            service = service,
+            amount = 23,
+            vat_percent = 0.22,
+            discount = 0,
+            invoice_period = 12,
+            subscribed_on_0 = DATE_CHOICES[14][0].strftime(self.DATE_FMT_YMG), 
+            subscribed_on_1 = "00:00:00", 
+            subscribed_until = DATE_CHOICES[15][0].strftime(self.DATE_FMT),
+            note = "note",
+        )
+
+        if logged_in:
+            self._check_for_redirect_response(response,is_ajax=False)
+
+            try:
+                subscription = ServiceSubscription.objects.get(
+                    pk=last_sub_pk+1
+                )
+            except ServiceSubscription.DoesNotExist as e:
+                subscription = False
+
+            self.assertTrue(subscription)
+
+
+        else:
+            self._check_for_redirect_response(response)
+
+    def test_client_service_subscripton_payment_create(self, query_string=""):
+        """
+        """
+     
+        # login with client
+        logged_in = self._admin_login()
+
+        self.assertTrue(logged_in)
+
+        #last_payment = ServiceSubscriptionPayment.objects.order_by("-id").first
+        customer = self.subscription.customer.pk
+        service = self.subscription.service.pk
+        response = self._do_POST_service_subscripton_payment_create(
+            customer = customer,
+            service = service,
+            amount = self.subscription.service.amount,
+            vat_percent = self.subscription.vat_percent,
+            discount = self.subscription.discount,
+            paid_for = DATE_CHOICES[10][0].strftime(self.DATE_FMT),
+            note = "note",
+        )
+
+        if logged_in:
+            self._check_for_redirect_response(response,is_ajax=False)
+
+            try:
+                sub_payment = ServiceSubscriptionPayment.objects.get(
+                    pk=self.payment.pk+1
+                )
+            except ServiceSubscriptionPayment.DoesNotExist as e:
+                sub_payment = False
+
+            self.assertTrue(sub_payment)
+
+            updated_subscription = ServiceSubscription.objects.get(pk=self.subscription.pk)
+            self.assertEqual(sub_payment.paid_on,updated_subscription.last_paid_on) 
+            self.assertEqual(sub_payment.paid_for,updated_subscription.last_paid_for) 
+
+        else:
+            self._check_for_redirect_response(response)
 
     def test_client_service_subscripton_payment_delete(self, payment=None, query_string=""):
         """
